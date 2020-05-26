2b414f48b1b29e17b89c2a28dd02a467684c9f47
Brayden Killeen <braydenmkilleen@gmail.com>
2020-05-26 16:16:40 +1000

    setup basic chat functionality

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
 import express from 'express';
 import http from 'http';
+import socketio from 'socket.io';
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-const server = http.createServer(app);
+io.on('connection', socket => {
+  console.log('user connected');
+
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 server.listen(3000, () => console.log(`listening on port ${3000}`));
