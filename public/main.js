a2ea06325bf0f70957c2bf1f7930f094e163170c
Brayden Killeen <braydenmkilleen@gmail.com>
2020-05-26 22:09:42 +1000

    Better dev build process and moved static files to public dir

---
+++ public/main.js
@@ -0,0 +1,55 @@
+const socket = io();
+
+const messages = document.querySelector('.messages');
+// const form = document.querySelector('.chat-form');
+// const chatInput = document.querySelector('.chat-form-input');
+const usernameInput = document.querySelector('.username');
+
+let username = '';
+
+function addChatMessage(message, options = {}) {}
+
+usernameInput.addEventListener('keyup', event => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    username = usernameInput.value;
+    socket.emit('add_user', username);
+
+    document.querySelector('.login-container').classList.add('hidden');
+
+    document.querySelector('.chat-container').classList.remove('hidden');
+  }
+});
+
+chatForm.addEventListener(
+  'submit',
+  event => {
+    event.preventDefault();
+
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `${username}: ${message}`;
+    messages.append(newMessage);
+
+    socket.emit('chat_message', { message: chatInput.value });
+    chatInput.value = '';
+
+    return false;
+  },
+  false
+);
+
+/**
+ * Socket events
+ */
+socket.on('user_joined', ({ username }) => {
+  const systemMessage = document.createElement('li');
+  systemMessage.classList.add('system-message');
+  systemMessage.innerHTML = `${username} joined the chat`;
+  messages.append(systemMessage);
+});
+
+socket.on('new_message', ({ username, message }) => {
+  const newMessage = document.createElement('li');
+  newMessage.innerHTML = `${username}: ${message}`;
+  messages.append(newMessage);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
 import express from 'express';
 import http from 'http';
 import socketio from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 io.on('connection', socket => {
-  io.emit('user connection', 'A new user has connected.');
-
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('add_user', username => {
+    socket.username = username;
+    socket.broadcast.emit('user_joined', { username: socket.username });
   });
 
-  socket.on('disconnect', () => {
-    io.emit('user connection', 'A user has disconnected.');
+  socket.on('new_message', data => {
+    socket.broadcast.emit('new_message', {
+      username: socket.username,
+      message: data,
+    });
   });
+
+  // socket.on('disconnect', () => {
+  //   io.emit('user connection', 'A user has disconnected.');
+  // });
 });
 
 server.listen(3000, () => console.log(`listening on port ${3000}`));
